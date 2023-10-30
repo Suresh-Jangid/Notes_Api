b671ec58f3d97c39ebf6ec9c1fa432ed7313afd1
suresh jangid <sureshjangid737@gmail.com>
2023-10-31 00:24:24 +0530

    api commit

---
+++ src/api/notes-app/routes/notes-app.ts
@@ -0,0 +1,7 @@
+/**
+ * notes-app router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::notes-app.notes-app');
---
+++ src/api/notes-app/services/notes-app.ts
@@ -0,0 +1,7 @@
+/**
+ * notes-app service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::notes-app.notes-app');
