28ff10fd2f4d62e4768ddb6b8081d6e06c3f5763
suresh jangid <sureshjangid737@gmail.com>
2023-11-19 23:57:35 +0530

    Auto-generated commit

---
+++ src/api/notes-app/controllers/notes-app.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * notes-app controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::notes-app.notes-app');
---
+++ src/api/notes-app/routes/notes-app.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * notes-app router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+//
+exports.default = strapi_1.factories.createCoreRouter('api::notes-app.notes-app');
